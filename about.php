f98dbff895bf0c9c96c0bf4295729e1033930957
Chris Stapler <cstapler@iastate.edu>
2017-08-29 11:33:39 -0500

    Add placeholders for member photos

--- about.php
+++ about.php
@@ -13,41 +13,81 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-xs-12 col-md-12">
+          <div class="col-xs-12 col-md-6">
             <h4>Chandler Chockalingam</h4>
             <p> I am studying Software Engineering. I have completed three software engineering internships; two with UTC Aerospace Systems and one with Boeing. During my two internships with UTC Aerospace Systems, I worked on projects related to embedded systems and database management. During my internship this past summer with Boeing, I worked on a deep learning project that included writing Python code to build neural networks to analyze flight data. I have completed coursework at Iowa State in embedded systems, database management, operating systems, user interfaces, software architecture, and software project management.
             </p>
           </div>
+          <div class="col-xs-12 col-md-6">
+            <img src="http://s3.thingpic.com/images/ZT/2Wxc6rj7mAfde18uRHGZvaM1.jpeg" class="img-rounded img-responsive">
+          </div>
         </div>
         <div class="row">
           <div class="col-xs-12 col-md-12">
+            <hr>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 col-md-6">
             <h4>Victor Da Silva</h4>
             <p>I am studying Computer Engineering. I have had two summer internships working in the Information Technology Department at Union Pacifc Railroad. At Union Pacific, I worked on projects that included large amounts of database connections and work with properly querying those databases. I also worked on a project that involved the production of an eclipse program that was written in Java. At Iowa State, I have worked with web development, embedded systems, and operating systems.
             </p>
           </div>
+          <div class="col-xs-12 col-md-6">
+            <img src="https://static.pexels.com/photos/33045/lion-wild-africa-african.jpg" class="img-rounded img-responsive">
+          </div>
         </div>
         <div class="row">
           <div class="col-xs-12 col-md-12">
+            <hr>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 col-md-6">
             <h4>Josua Gonzales-Neal</h4>
             <p></p>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12 col-md-12">
-            <h4>Logan Highland</h4>
-            <p>  I am studying Software Engineering. I have just finished my first Software Engineering internship working with the security company WatchGuard Technologies working on UI/UX and Web Development. The work that I did mostly involved using Python, XSLT, and web development tools such as JavaScript, HTML, and PHP. At Iowa State I have worked with embedded systems, web development, and Android Development. I am also working on a side project that will use Arduino boards and a Google Home/Amazon Echo to communicate with a self built machine to pour mixed drinks            </p>
+          <div class="col-xs-12 col-md-6">
+            <img src="https://www.stockvault.net/data/2008/12/29/107108/thumb16.jpg" class="img-rounded img-responsive">
           </div>
         </div>
         <div class="row">
           <div class="col-xs-12 col-md-12">
+            <hr>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 col-md-6">
+            <h4>Logan Highland</h4>
+            <p>  I am studying Software Engineering. I have just finished my first Software Engineering internship working with the security company WatchGuard Technologies working on UI/UX and Web Development. The work that I did mostly involved using Python, XSLT, and web development tools such as JavaScript, HTML, and PHP. At Iowa State I have worked with embedded systems, web development, and Android Development. I am also working on a side project that will use Arduino boards and a Google Home/Amazon Echo to communicate with a self built machine to pour mixed drinks            </p>
+          </div>
+          <div class="col-xs-12 col-md-6">
+            <img src="https://images4.alphacoders.com/780/78068.jpg" class="img-rounded img-responsive">
+          </div>
+        </div>
+      <div class="row">
+          <div class="col-xs-12 col-md-12">
+            <hr>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 col-md-6">
             <h4>Jason Ramirez</h4>
             <p>
               I am studying Computer Engineering. My industry experience is within both the manufacturing and aerospace industry. My first internship after Freshman year was with a pharma company working on a tool, written in VBA, for the human resources department. My second internship was with Procter & Gamble. There I utilized C# and reinforcement learning to make a machine on the factory floor faster by 25% (Compared to traditional humans selecting the speeds and timers). My last internship was with The Boeing Company. There I worked on an agile team making applications for both internal and external customers. My biggest project there was working on a data analytics application that dealt with unsupervised learning (text clustering to be exact) written in Python.
             </p>
           </div>
+          <div class="col-xs-12 col-md-6">
+            <img src="https://static.pexels.com/photos/145939/pexels-photo-145939.jpeg" class="img-rounded img-responsive">
+          </div>
         </div>
         <div class="row">
           <div class="col-xs-12 col-md-12">
+            <hr>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 col-md-6">
             <h4>Christopher Stapler</h4>
             <p> I am a senior studying Computer Engineering.This past summer,
               I was at the University of Florida working on IPOP, an open source
@@ -62,6 +102,9 @@
                    worked at the Office of Student Financial Aid at Iowa State
                    for a full year before interning at FHLBDM.
             </p>
+          </div>
+          <div class="col-xs-12 col-md-6">
+            <img src="http://elelur.com/data_images/mammals/cougar/cougar-07.jpg" class="img-rounded img-responsive">
           </div>
         </div>
       </div>
