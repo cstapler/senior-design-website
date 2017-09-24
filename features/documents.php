d0773780b925c28cce3db591a6b6b30940d2d3a0
Chris Stapler <cstapler@iastate.edu>
2017-09-24 14:38:50 -0500

    Add project plan

--- features/documents.php
+++ features/documents.php
@@ -7,11 +7,24 @@
     <main role="main" id="main-content">
     	<div class="container">
         <div class="row">
-        	<div class="col-xs-12 col-md-12">
+        	<div class="col-xs-12">
             <h3>Design Documents</h3>
           </div>
         </div>
-              </div>
+        <div class="row">
+          <div class="col-xs-12">
+            <hr>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 col-md-8 col-md-offset-2">
+            <div class="list-group">
+              <a class="list-group-item"
+                  href="/docs/project_plan.pdf">Project Plan</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
     <!-- /main -->
 
